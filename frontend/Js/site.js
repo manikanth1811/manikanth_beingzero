8e321be456052d198396fec469ff75dced99b98d
manikanth18 <manikanthpolice18@gmail.com>
2019-12-20 15:22:07 +0530

    Fixing Alert and frontend folder name

--- frontend/Js/site.js
+++ frontend/Js/site.js
@@ -2,7 +2,7 @@
 
 //1. how to make a backend call
 $.getJSON('/data',function(d){
-    alert(JSON.stringify(d));
+    //alert(JSON.stringify(d));
     $('#name').text(d.name);
     $('#college').text(d.college);
     $('#number').text(d.regno);
