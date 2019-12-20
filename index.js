33134ae15c50bb492ac29b4b1d32ad07cf919541
manikanth18 <manikanthpolice18@gmail.com>
2019-12-20 14:49:46 +0530

    Adding Project Files

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+//tells express to serve the js or css files form fronted folder
+app.use(express.static('frontend'))
+app.get('/', (req, res) => res.sendFile(__dirname+'/frontend/html/home.html'))
+app.get('/login', (req, res) => res.send('Login!'))
+app.get('/Register', (req, res) => res.send('Register!'))
+var student={
+    "name":"manikanth",
+    "college":"vnrvjiet",
+    "regno":"17071A12A2"
+};
+app.get('/data',(req,res)=>res.json(student))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
